209d864909b2c4e5822c4fc14f287c82664eca53
zrobit <luismigre47@gmail.com>
2014-08-30 16:25:34 -0500

    add initial url

--- begin/urls.py
+++ begin/urls.py
@@ -4,9 +4,13 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'begin.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'posts.views.home', name='home'),
+
+    # url(r'^categories/$', 'posts.views.categories', name='categories'),
+
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<slug>[a-zA-Z0-9\-]+)/$',
+        'posts.views.detail', name='detail')
 )
