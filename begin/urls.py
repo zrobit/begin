c4b46e27a5fe8af908ffcff494fa58b79b4dcdc9
zrobit <luismigre47@gmail.com>
2014-09-27 21:38:10 -0500

    çhange urls for django 1.7

--- begin/urls.py
+++ begin/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-admin.autodiscover()
+
+# admin.autodiscover()
 
 urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'begin.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 
