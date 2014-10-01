a7e23886c46d542972f09cbd444eb3f9ad560127
zrobit <luismigre47@gmail.com>
2014-09-30 23:56:47 -0500

    add registers for models

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post, Category, Tag, Author
+
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Author)
